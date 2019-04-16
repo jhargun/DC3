e1b0a63bb0c16ccc0f10232e315ab7ba00e09cad
Jaswin Hargun <jhargun20@andover.edu>
2019-04-16 00:46:48 -0400

    modified:   madlibs/views.py

--- madlibs/views.py
+++ madlibs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
-from .forms import form1
+from .forms import form1, form2
 # Create your views here.
 
 def home(request):

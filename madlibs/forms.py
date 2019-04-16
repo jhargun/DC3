9d4ad84e461e1354e2fba7e37ad077f9f5d7e578
Jaswin Hargun <jhargun20@andover.edu>
2019-04-16 00:40:01 -0400

    modified:   madlibs/forms.py new file:   madlibs/templates/madlibs/Home.html renamed:    madlibs/templates/madlibs/form1.html -> madlibs/templates/madlibs/lib1.html new file:   madlibs/templates/madlibs/lib2.html modified:   madlibs/views.py

--- madlibs/forms.py
+++ madlibs/forms.py
@@ -12,3 +12,15 @@ class form1(forms.Form):
     h = forms.CharField(label='Adjective', max_length=100)
     i = forms.CharField(label='Adjective', max_length=100)
     j = forms.CharField(label='Adjective', max_length=100)
+
+class form2 (forms.Form):
+    a = forms.CharField(label='Silly word', max_length=100)
+    b = forms.CharField(label='Last name', max_length=100)
+    c = forms.CharField(label='Illness', max_length=100)
+    d = forms.CharField(label='Noun (plural)', max_length=100)
+    e = forms.CharField(label='Adjective', max_length=100)
+    f = forms.CharField(label='Adjective', max_length=100)
+    g = forms.CharField(label='Silly word', max_length=100)
+    h = forms.CharField(label='Place', max_length=100)
+    i = forms.CharField(label='Number', max_length=100)
+    j = forms.CharField(label='Adjective', max_length=100)
--- madlibs/views.py
+++ madlibs/views.py
@@ -5,24 +5,28 @@ from .forms import form1
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Test")
+    return render(request, 'madlibs/Home.html')
 
 def madlib(request, num):
-    #return render(request, 'madlibs/form1.html')
     if request.method == 'POST':
         if num == 1:
             form = form1(request.POST)
-            if form.is_valid():
-                #Made a for loop to clean data and add to context
-                vars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-                context = {'j': form.cleaned_data['j']}
-                for i in range(len(vars) - 1):
-                    context[vars[i]] = form.cleaned_data[vars[i]]
-                #return HttpResponse(context)
-                return render(request, 'madlibs/form1.html', context)
+        elif num == 2:
+            form = form2(request.POST)
+        if form.is_valid():
+            #Made a for loop to clean data and add to context, works since both templates have 10 inputs
+            vars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+            context = {'j': form.cleaned_data['j']}
+            for i in range(len(vars) - 1):
+                context[vars[i]] = form.cleaned_data[vars[i]]
+            if num == 1:
+                return render(request, 'madlibs/lib1.html', context)
+            return render(request, 'madlibs/lib2.html', context)
     else:
         if num == 1:
             form = form1()
+        elif num == 2:
+            form = form2()
         else:
-            return HttpResponse('Not a valid madlib number! Valid numbers: 1')
+            return HttpResponse('Not a valid madlib number! Valid numbers: 1, 2')
     return render(request, 'madlibs/Form.html', {'form': form})
